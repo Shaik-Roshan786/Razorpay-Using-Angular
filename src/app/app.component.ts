539c959f61f47474801a6d26ac7e6d8b0a269228
Shaik-Roshan786 <roshanshaik2000@gmail.com>
2023-08-07 16:40:51 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+declare var Razorpay: any;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'razorpay-angular';
+
+  payNow() {
+    const RozarpayOptions = {
+      description: 'Sample Rozorpay demo',
+      currency: 'INR',
+      amount: 10000,
+      name: 'Sai',
+      key: 'rzp_test_W3pHnVCWYA1Ulx',
+      image: 'https://i.imgur.com/FApqk3D.jpeg',
+      prefill: {
+        name: 'sai kumar',
+        email: 'sai@gmail.com',
+        phone: '9898989898',
+      },
+      theme: {
+        color: '#6466e3'
+      },
+      modal: {
+        ondismiss: () => {
+          console.log('dismissed')
+        } 
+      }
+    }
+
+    const successCallback = (paymentid: any) => {
+      console.log(paymentid);
+    }
+    
+    const failureCallback = (e: any) => {
+      console.log(e);
+  }
+
+  // Rozorpay.open(RozarpayOptions,successCallback, failureCallback)
+  Razorpay.open(RozarpayOptions,successCallback, failureCallback)
+  }
 }
